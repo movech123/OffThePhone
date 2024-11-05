93982e9803550bd8d0063c56f1b7504da0f52531
SDenz22 <sdenzin80@gmail.com>
2024-11-04 19:28:58 -0600

    Added .xml files

--- app/src/main/java/com/cs407/offthephone/Tasks.kt
+++ app/src/main/java/com/cs407/offthephone/Tasks.kt
@@ -1,4 +1,20 @@
-package com.cs407.offthephone;
+package com.cs407.offthephone
 
-public class Tasks {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Tasks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tasks)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
