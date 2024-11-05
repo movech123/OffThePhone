c588189fdb6beaf0ab01c9c1e838f0ec576067c5
movech123 <vmodalla@gmail.com>
2024-11-04 19:14:06 -0600

    nav

--- app/src/main/java/com/cs407/offthephone/MainActivity.kt
+++ app/src/main/java/com/cs407/offthephone/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +18,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    val navController = rememberNavController()
+
 }
