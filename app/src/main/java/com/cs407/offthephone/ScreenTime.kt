2097dd07561d5a32af505c4e0cd25c67b3b0f3ee
movech123 <vmodalla@gmail.com>
2024-11-06 16:37:42 -0600

    Merge branch 'main' of https://github.com/movech123/OffThePhone
    # Conflicts:
    #	README.md

--- app/src/main/java/com/cs407/offthephone/ScreenTime.kt
+++ app/src/main/java/com/cs407/offthephone/ScreenTime.kt
@@ -1,20 +1,32 @@
 package com.cs407.offthephone
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ScreenTime : AppCompatActivity() {
+
+    private lateinit var SettingsButton : Button
+    private lateinit var  HomeButton : Button
+    private lateinit var  TasksButton : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_screen_time)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_view)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        SettingsButton = findViewById(R.id.settings_button)
+        HomeButton = findViewById(R.id.home_button)
+        TasksButton = findViewById(R.id.task_button)
     }
+
+
 }
