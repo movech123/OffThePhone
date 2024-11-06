3b78501d287e1e647748122804ed0ae88539621f
movech123 <vmodalla@gmail.com>
2024-11-06 16:40:17 -0600

    Merge branch 'main' of https://github.com/movech123/OffThePhone
    # Conflicts:
    #	README.md

--- app/src/main/java/com/cs407/offthephone/ScreenTime.kt
+++ app/src/main/java/com/cs407/offthephone/ScreenTime.kt
@@ -1,6 +1,7 @@
 package com.cs407.offthephone
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
