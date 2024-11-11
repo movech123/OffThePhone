74863a9b23970a10ced1e5439d84f26047e3840d
SDenz22 <sdenzin80@gmail.com>
2024-11-11 16:21:05 -0600

    Added Homepage XML, Schedule item .kt and .xml to dynamically display a schedule

--- app/src/main/java/com/cs407/offthephone/Homepage.kt
+++ app/src/main/java/com/cs407/offthephone/Homepage.kt
@@ -1,21 +1,36 @@
 package com.cs407.offthephone
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class Homepage : AppCompatActivity() {
+    private lateinit var date_box: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // set date string in the layout
+        date_box = findViewById(R.id.current_date)
+        val sdf = SimpleDateFormat("dd-MM-yyyy")
+        val currentDate = sdf.format(Date())
+        date_box.text = currentDate
+
+        // Sample data for schedule items
+        val scheduleList = listOf(
+            ScheduleItem("Study", "12:00pm - 2:00pm"),
+            ScheduleItem("Cook", "2:00pm - 5:00pm"),
+            ScheduleItem("407 Meeting", "5:00pm - 8:00pm")
+        )
+        // Set up RecyclerView with adapter
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewSchedule)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ScheduleAdapter(scheduleList)
     }
 
     private fun setupUI() {
